e0c1f275245a8a7521e575205475cb3bce41458d
Betank <bennitank@gmail.com>
2017-03-26 19:27:30 +0200

    dockerfiles and build

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+for d in tweet-command-service tweet-query-service user-command-service user-query-service ; do
+    cd $d
+
+    echo -n "Building $d"
+    CGO_ENABLED=0 GOOS=linux go build -o $d-docker
+    
+    echo -n "."
+    docker build -t "twitterclone-$d" --quiet .
+
+    cd ..
+done
+
+cd api-gateway/server
+
+echo -n "Building api-gateway"
+CGO_ENABLED=0 GOOS=linux go build -o api-gateway-docker
+    
+echo -n "Building api-gateway"
+cd ..
+npm install --quiet
+
+echo -n "."
+npm run build --quiet
+
+echo -n "."
+docker build -t "twitterclone-api-gateway" --quiet .
