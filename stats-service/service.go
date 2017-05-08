625d2da9cb1cc560fef08ce38fff8b59f9e47fec
Betank <bennitank@gmail.com>
2017-05-08 22:09:06 +0200

    stats tests

--- stats-service/service.go
+++ stats-service/service.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"context"
 	"encoding/json"
 	"log"
 	"net/http"
 	"os"
 
-	"github.com/SermoDigital/jose/jws"
-	"github.com/gorilla/mux"
 	nsq "github.com/nsqio/go-nsq"
 )
 
@@ -22,12 +19,6 @@ type stats struct {
 	Tweets   int `json:"tweets" bson:"tweets"`
 }
 
-type tokenHandler struct {
-	next http.Handler
-}
-
-type statsHandler struct{}
-
 var store Storage
 var createTweetConsumer, deleteTweetConsumer, newUserConsumer *nsq.Consumer
 var nsqAddress string
@@ -37,40 +28,8 @@ func main() {
 	store = NewMongoStorage()
 	setupNSQ()
 
-	router := mux.NewRouter()
-	router.StrictSlash(true)
-	router.Handle("/api/stats/", needsToken(&statsHandler{})).Methods("GET")
-	router.HandleFunc("/api/stats/{userId}", statsForUser).Methods("GET")
-
-	http.Handle("/", router)
+	http.Handle("/", Router())
 	http.ListenAndServe(":8080", nil)
-}
-
-func (handler *statsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	respondData(w, r, store.GetStatsByUserID(r.Context().Value("userID").(string)))
-}
-
-func statsForUser(w http.ResponseWriter, r *http.Request) {
-	respondData(w, r, store.GetStatsByUserID("12345"))
-}
-
-func (auth *tokenHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	jwt, err := jws.ParseJWTFromRequest(r)
-	if err != nil {
-		w.WriteHeader(http.StatusUnauthorized)
-		return
-	}
-	ctx := context.WithValue(r.Context(), "userID", jwt.Claims().Get("userID"))
-
-	auth.next.ServeHTTP(w, r.WithContext(ctx))
-}
-
-func needsToken(handler http.Handler) http.Handler {
-	return &tokenHandler{handler}
-}
-
-func respondData(w http.ResponseWriter, r *http.Request, data interface{}) error {
-	return json.NewEncoder(w).Encode(data)
 }
 
 func setupNSQ() {
